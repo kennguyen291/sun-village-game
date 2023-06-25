7b5a62276bd57166ad1d63c3fe3db3c0f7474c00
Ken Nguyen <khangnguyen291@gmail.com>
2023-06-25 14:51:41 +1000

    import and render map

---
+++ index.js
@@ -0,0 +1,15 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillRect(0,0,canvas.width, canvas.height)
+
+const image = new Image()
+image.src = './img/World02 Map.png'
+
+
+image.onload = () => {
+    c.drawImage(image,-1430, -700)
+}
